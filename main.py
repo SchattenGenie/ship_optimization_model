367c8ae729da82cf78054e28ea94f78b230d0a07
vbelavin <belavin@phystech.edu>
2019-08-28 16:00:32 +0300

    async running of docker

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import config
 from flask import Flask, jsonify
 from flask import request as flask_request
+from threading import Thread
 import config
 import run_ship
 import json
-
+import uiid
 
 app = Flask(__name__)
 
@@ -14,14 +15,15 @@ app = Flask(__name__)
 @app.route('/simulate', methods=['POST'])
 def simulate():
     magnet_config = json.loads(flask_request.data)
-    result = run_ship.run_simulation(magnet_config)
-    return result
+    job_uiid = str(uuid.uuid4())
+    Thread(target=run_ship.run_simulation, kwargs=dict(magnet_config=magnet_config, job_uiid=job_uiid)).start()
+    return job_uiid
 
 # TODO: lazy evaluation
 @app.route('/retrieve_result', methods=['POST'])
 def retrieve_result():
     data = json.loads(flask_request.data)
-    result = get_result(data['uiid'])
+    result = run_ship.get_result(data['uiid'])
     # if result is None
     # that there are two possible situations
     # calculation is not finished yet
--- run_ship.py
+++ run_ship.py
@@ -16,28 +16,27 @@ import docker
 from shutil import copy
 import numpy as np
 import config
+from redis import Redis
 
+
+redis = Redis()
 client = docker.from_env()
 
 
 def create_job(host_dir, container_dir, command):
     container = client.containers.run(
         image='vbelavin/ship_simple_model',
-        remove=False,
-        detach=False,
+        detach=True,
         command=command,
-        tty=True,
-        stdin_open=True,
-        stderr=True,
         volumes={container_dir: {'bind': host_dir, 'mode': 'rw'}}
     )
     return container
 
 
-def run_simulation(magnet_config):
+def run_simulation(magnet_config, job_uiid):
     # make random directory for ship docker
     # to store input files and output files
-    input_dir = 'input_dir_{}'.format(str(uuid.uuid4()))
+    input_dir = 'input_dir_{}'.format(job_uiid)
     host_dir = '{}/{}'.format(config.CONTAINER_DIRECTORY, input_dir)
     host_dir = os.path.abspath(host_dir)
     host_outer_dir = '{}/{}'.format(config.HOST_DIRECTORY, input_dir)
@@ -58,28 +57,41 @@ def run_simulation(magnet_config):
     # TODO: use python docker sdk
     num_repetitions = 1000
     command = "alienv setenv -w /sw FairShip/latest -c /run_simulation.sh {}".format(num_repetitions)
-    create_job(host_dir=host_dir, container_dir=container_dir, command=command)
-    """
-    process = subprocess.Popen([
-        "docker", "run", "-v",
-        "{}:{}:rw".format(host_outer_dir, container_dir),
-        "vbelavin/ship_simple_model"
-    ],
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    container = create_job(host_dir=host_dir, container_dir=container_dir, command=command)
+    result = {
+        'uiid': job_uiid,
+        'container_id': container.id,
+        'container_status': container.status
+    }
 
-    for line in process.stdout:
-        print(line)
-    process.wait()
-    """
+    redis.hmset(job_uiid, result)
+    container.wait()
+
     muons_momentum_plus = np.load('{0}/output_mu/muons_momentum.npy'.format(host_dir))
     muons_momentum_minus = np.load('{0}/output_antimu/muons_momentum.npy'.format(host_dir))
 
     veto_points_plus = np.load('{0}/output_mu/veto_points.npy'.format(host_dir))
     veto_points_minus = np.load('{0}/output_antimu/veto_points.npy'.format(host_dir))
 
+    container.reload()
     result = {
+        'uiid': job_uiid,
+        'container_id': container.id,
+        'container_status': container.status,
         'muons_momentum': np.concatenate([muons_momentum_plus, muons_momentum_minus], axis=0).tolist(),
         'veto_points': np.concatenate([veto_points_plus, veto_points_minus], axis=0).tolist()
     }
-
+    redis.hmset(job_uiid, result)
     return result
+
+
+def get_result(job_uiid):
+    result = redis.hgetall(job_uiid)
+    if result is None:
+        return 'No key'
+    if result['container_status'] == 'exited':
+        return result
+    else:
+        container = client.get(result['container_id'])
+        container.reload()
+        return container.status
