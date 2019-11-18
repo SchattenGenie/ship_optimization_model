bd2dc7d9b3ecf4a5b96a2560056a103093937516
vbelavin <belavin@phystech.edu>
2019-11-18 20:23:53 +0300

    bug fixing

--- control/run_job.py
+++ control/run_job.py
@@ -39,7 +39,7 @@ def status_checker(job):
     return 'wait'
 
 
-def run_simulation(magnet_config, job_uuid):
+def run_simulation(magnet_config, job_uuid, n_events, first_event):
     # make random directory for ship docker
     # to store input files and output files
     input_dir = 'input_dir_{}'.format(job_uuid)
@@ -64,15 +64,14 @@ def run_simulation(magnet_config, job_uuid):
 
     JOB_SPEC = deepcopy(config.JOB_SPEC)
     job_spec_config_file = os.path.join(flask_host_dir, "job_spec.json")
-    with open(magnet_config_path, 'w', encoding='utf-8') as f:
-        json.dump(JOB_SPEC, f, ensure_ascii=False, indent=4)
     # JOB_SPEC["spec"]["containers"][0]["volumeMounts"][0]["mountPath"] = SHIP_CONTAINER_DIRECTORY
     JOB_SPEC["spec"]["template"]["spec"]["volumes"][0]["hostPath"]["path"] = host_outer_dir
     JOB_SPEC["metadata"]["name"] = "ship-job-{}".format(job_uuid)
     JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(",".join(map(str, magnet_config)))
     JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(n_events)
     JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(0)
-
+    with open(magnet_config_path, 'w', encoding='utf-8') as f:
+        json.dump(JOB_SPEC, f, ensure_ascii=False, indent=4)
     job = pykube.Job(api, json.dumps(JOB_SPEC))
     job.create()
 
