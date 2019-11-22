ad5baa6b3b97a59284024b1049a12115cf8f9e03
vbelavin <belavin@phystech.edu>
2019-11-22 17:39:38 +0300

    split jobs + http exceptions

--- control/run_job.py
+++ control/run_job.py
@@ -38,7 +38,7 @@ def status_checker(job):
     return 'wait'
 
 
-def run_simulation(magnet_config, job_uuid, n_events, first_event):
+def run_simulation(magnet_config, job_uuid, n_jobs):
     # make random directory for ship docker
     # to store input files and output files
     input_dir = 'input_dir_{}'.format(job_uuid)
@@ -60,55 +60,94 @@ def run_simulation(magnet_config, job_uuid, n_events, first_event):
     }
     redis.set(job_uuid, json.dumps(result))
 
-    JOB_SPEC = deepcopy(config.JOB_SPEC)
     job_spec_config_file = os.path.join(flask_host_dir, "job_spec.json")
-    # JOB_SPEC["spec"]["containers"][0]["volumeMounts"][0]["mountPath"] = SHIP_CONTAINER_DIRECTORY
-    print(host_outer_dir)
-    JOB_SPEC["spec"]["template"]["spec"]["volumes"][0]["hostPath"]["path"] = host_outer_dir
-    JOB_SPEC["metadata"]["name"] = "ship-job-{}".format(job_uuid)
-    JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(",".join(map(str, magnet_config)))
-    JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(str(n_events))
-    JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(str(0))
-    print(JOB_SPEC)
-    with open(job_spec_config_file, 'w', encoding='utf-8') as f:
-        json.dump(JOB_SPEC, f, ensure_ascii=False, indent=4)
-    job = pykube.Job(api, JOB_SPEC)
-    job.create()
+    jobs = []
+    uuids = []
+
+    chunk_size = 1 + config.EVENTS_TOTAL // n_jobs
+    start_event_num = 0
+    for part_number in range(n_jobs):
+        JOB_SPEC = deepcopy(config.JOB_SPEC)
+        flask_host_dir_part = '{}/part_{}'.format(flask_host_dir, part_number)
+        host_outer_dir_part = '{}/part_{}'.format(host_outer_dir, part_number)
+        os.mkdir(flask_host_dir_part)
+        magnet_config_path_part = os.path.join(flask_host_dir_part, "magnet_params.json")
+        with open(magnet_config_path_part, 'w', encoding='utf-8') as f:
+            json.dump(magnet_config, f, ensure_ascii=False, indent=4)
+        per_job_uuid = "{}-{}".format(job_uuid, part_number)
+        JOB_SPEC["spec"]["template"]["spec"]["volumes"][0]["hostPath"]["path"] = host_outer_dir_part
+        JOB_SPEC["metadata"]["name"] = "ship-job-{}".format(per_job_uuid)
+        JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(",".join(map(str, magnet_config)))
+        JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(str(chunk_size))
+        JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(str(start_event_num))
+        JOB_SPEC["spec"]["template"]["spec"]["containers"][0]["command"].append(config.DATA_FILE)
+        print(JOB_SPEC)
+        job_spec_config_file = os.path.join(flask_host_dir_part, "job_spec.json")
+        with open(job_spec_config_file, 'w', encoding='utf-8') as f:
+            json.dump(JOB_SPEC, f, ensure_ascii=False, indent=4)
+        job = pykube.Job(api, JOB_SPEC)
+        job.create()
+        jobs.append(job)
+        uuids.append(per_job_uuid)
+        start_event_num += chunk_size
 
     result = {
-        'uuid': job_uuid,
-        'container_id': job.obj['metadata']['name'],
-        'container_status': job.obj['status'],
+        'uuid': uuids,
+        'container_id': [job.obj['metadata']['name'] for job in jobs],
+        'container_status': [job.obj['status'] for job in jobs],
         'message': None
     }
     redis.set(job_uuid, json.dumps(result))
     time.sleep(1.)
-    job.reload()
+    [job.reload() for job in jobs]
     print(os.listdir(flask_host_dir))
-    status = 'wait'
+    finished = False
+    print([job.obj for job in jobs])
     try:
-        while status == 'wait':
+        while not finished:
+            statuses = []
             time.sleep(10)
-            job.reload()
-            status = status_checker(job=job)
-        if status == 'failed':
-            raise(ValueError("JOB FAILED!!!!"))
-
-        print(job.obj)
+            print([job.obj['status'] for job in jobs])
+            for job in jobs:
+                status = 'wait'
+                try:
+                    job.reload()
+                    status = status_checker(job=job)
+                except requests.exceptions.HTTPError as e:
+                    # except only internet errors
+                    print(e, traceback.print_exc())
+                statuses.append(status)
+            if 'wait' in statuses:
+                finished = False
+            else:
+                finished = True
         time.sleep(60)
         print(os.listdir(flask_host_dir))
-        with open('{}/{}'.format(flask_host_dir, 'optimise_input.json'), 'r') as j:
-            optimise_input = json.loads(j.read())
 
+        # collect data from succesfully finished jobs
+        optimise_inputs = []
+        for part_number, job in enumerate(jobs):
+            if status_checker(job=job) == 'succeeded':
+                with open('{}/{}'.format("{}/part_{}".format(flask_host_dir, part_number), 'optimise_input.json'), 'r') as j:
+                    optimise_input = json.loads(j.read())
+                optimise_inputs.append(optimise_input)
+
+        kinematics = sum([optimise_input["kinematics"] for optimise_input in optimise_inputs], [])
+        params = sum([optimise_input["params"] for optimise_input in optimise_inputs], [])
+        veto_points = sum([optimise_input["veto_points"] for optimise_input in optimise_inputs], [])
+        l = sum([optimise_input["l"] for optimise_input in optimise_inputs], [])
+        w = sum([optimise_input["w"] for optimise_input in optimise_inputs], [])
+
+        print([job.obj for job in jobs])
         result = {
-            'uuid': job_uuid,
-            'container_id': job.obj['metadata']['name'],
-            'container_status': job.obj['status'],
-            'kinematics': optimise_input["kinematics"],
-            "params": optimise_input["params"],
-            "veto_points": optimise_input["veto_points"],
-            "l": optimise_input["l"],
-            "w": optimise_input["w"],
+            'uuid': uuids,
+            'container_id': [job.obj['metadata']['name'] for job in jobs],
+            'container_status': [job.obj['status'] for job in jobs],
+            'kinematics': kinematics,
+            "params": params,
+            "veto_points": veto_points,
+            "l": l,
+            "w": w,
             'message': None
         }
         redis.set(job_uuid, json.dumps(result))
@@ -116,9 +155,9 @@ def run_simulation(magnet_config, job_uuid, n_events, first_event):
     except Exception as e:
         print(e, traceback.print_exc())
         result = {
-            'uuid': job_uuid,
-            'container_id': job.obj['metadata']['name'],
-            'container_status': job.obj['status'],
+            'uuid': uuids,
+            'container_id': [job.obj['metadata']['name'] for job in jobs],
+            'container_status': [job.obj['status'] for job in jobs],
             'muons_momentum': None,
             'veto_points': None,
             'message': traceback.format_exc()
