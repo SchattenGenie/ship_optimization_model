a09202eeb9fd3a76a85ed18bed6d1ba2461a6fde
shir994 <shir994@gmail.com>
2019-12-06 20:58:28 +0000

    Trying to make tee logs work :(

--- ship/run_simulation.sh
+++ ship/run_simulation.sh
@@ -1,5 +1,6 @@
 ls -lthaR /root/host_directory/
-python run_simulation.py --shield_params $1 --n_events $2 --first_event $3 --file_name $4
+echo "BLAH" | tee /ship/shield_files/outputs/test.out
+time python run_simulation.py --shield_params $1 --n_events $2 --first_event $3 --file_name $4 | tee /ship/shield_files/outputs/ship_logs.txt
 ls -lthaR /ship/shield_files/
 ls -lthaR /ship/shield_files/outputs/
 ls -lthaR /root/host_directory/
